e362869059721cf1394c14003932355d1d67a32f
andrew.andrews <andrewardyour@gmail.com>
2024-11-11 17:39:05 +0500

    Realize sort_by_date function

--- src/processing.py
+++ src/processing.py
@@ -13,3 +13,21 @@ def filter_by_state(records: list, state="EXECUTED") -> list:
             new_records.append(dir)
 
     return new_records
+
+
+def sort_by_date(records: list, sort_option ='True') -> list:
+    """
+    Сортирует список словарей по убыванию(default) или возрастанию.
+    :param records: принимает список словарей с данными о бансковских операциях.
+    :param sort_option: True(default), отвечает за направление сортировки (на убывание по умолчанию).
+    :return: Отсортированный по дате список словарей.
+    """
+
+    sorted_records = []
+
+    if sort_option == 'True':
+        sorted_records = sorted(records, key=lambda record: record["date"], reverse=True)
+    elif sort_option == 'False':
+        sorted_records = sorted(records, key=lambda record: record["date"])
+
+    return sorted_records
